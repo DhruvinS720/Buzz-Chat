2b4ee87f81edb048d7f38570c203be513613a5db
Dhruvin Sorathiya <dhruvinsorathiya102.15@gmail.com>
2023-02-03 17:13:19 +0530

    changes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", clientRouter);
 app.use("/users", usersRouter);
-app.use("/client", usersRouter);
+app.use("/index", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
