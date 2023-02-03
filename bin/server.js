ea780893f6cd70b8d184f3a500694b43d82891d8
Dhruvin Sorathiya <dhruvinsorathiya102.15@gmail.com>
2023-02-03 12:01:21 +0530

    complete chat

--- bin/server.js
+++ bin/server.js
@@ -29,7 +29,19 @@ var users = {};
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (userName) => {
     users[socket.id] = userName;
-    socket.broadcast.emit("user-joined",userName);
+    socket.broadcast.emit("user-joined", userName);
+  });
+
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      userName: users[socket.id],
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", users[socket.id]);
+    delete users[socket.id];
   });
 });
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,44 @@
-console.log("hello world");
 const socket = io();
 var userName;
 
+const send_form = document.querySelector("#send_form");
 const message_area = document.querySelector(".message_area");
+const write_message = document.querySelector("#write_message");
 
 do {
   userName = prompt("Enter Your Name:");
 } while (!userName);
+
+const scrollbottom = () => {
+  message_area.scrollTop = message_area.scrollHeight;
+};
+
+send_form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const user_message = { message: write_message.value, userName: "You" };
+  append(user_message, "message-right");
+  socket.emit("send", write_message.value);
+  write_message.value = "";
+});
+
+const append = (data, position) => {
+  console.log("halu");
+  const div = document.createElement("div");
+  div.classList.add(position);
+  console.log(div);
+  const h2 = document.createElement("h2");
+  h2.classList.add("user");
+  h2.innerText = data.userName;
+  console.log(h2);
+  const p = document.createElement("p");
+  p.classList.add("message");
+  p.innerText = data.message;
+  console.log(p);
+  div.appendChild(h2);
+  div.appendChild(p);
+  message_area.appendChild(div);
+  scrollbottom();
+};
 
 const appendJoined = (message, position) => {
   const message_center = document.createElement("div");
@@ -16,16 +48,23 @@ const appendJoined = (message, position) => {
   joined.innerText = message;
   message_center.appendChild(joined);
   message_area.appendChild(message_center);
+  scrollbottom();
 };
 
 appendJoined("You joined the chat", "message-center");
-
-const append = () => {
-  const messageElement = document.getElementsByClassName();
-};
 
 socket.emit("new-user-joined", userName);
 
 socket.on("user-joined", (userName) => {
   appendJoined(`${userName} joined the chat`, "message-center");
+});
+
+socket.on("receive", (data) => {
+  append(data, "message-left");
+});
+
+socket.on("leave", (userName) => {
+  if (userName != null) {
+    appendJoined(`${userName} left the chat`, "message-center");
+  }
 });
